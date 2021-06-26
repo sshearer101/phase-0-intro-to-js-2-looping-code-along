af2896dc817eaaacbe54382c114eab13897777a2
Sam Shearer <sshearer101@gmail.com>
2021-06-25 20:14:22 -0600

    Completed aassignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+let messages = [];
+  for (let i = 0; i < names.length; i++) {
+  messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return messages;
+}
+
+
+
+
+function countDown (posNumber) {
+  while (posNumber >= 0) {
+    console.log (posNumber);
+    posNumber --;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
