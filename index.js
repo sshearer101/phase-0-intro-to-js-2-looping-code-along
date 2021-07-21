c6e94b28cc6be57d40774fdcc3256da59e1cec0b
Sam Shearer <sshearer101@gmail.com>
2021-07-21 16:07:12 -0600

    commit tree 4e6dd04da186cbc2d6f92b814c7b840651f40db0

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 
-function writeCards(names, event) {
-let messages = [];
-  for (let i = 0; i < names.length; i++) {
-  messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
-  }
-  return messages;
-}
 
 
 
 
-function countDown (posNumber) {
-  while (posNumber >= 0) {
-    console.log (posNumber);
-    posNumber --;
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(names, event) {
+// let messages = [];
+//   for (let i = 0; i < names.length; i++) {
+//   messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+//   }
+//   return messages;
+// }
+
+
+
+
+// function countDown (posNumber) {
+//   while (posNumber >= 0) {
+//     console.log (posNumber);
+//     posNumber --;
+//   }
+// }
 
 
 
